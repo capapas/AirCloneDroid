e307c43d239b3baebb2d71ff1d8b20d94b328e23
Daybay <kelmeni.f@gmail.com>
2014-07-02 17:55:55 +0200

    Download de dossier sous format zip

--- app/assets/html/public/javascript/app.js
+++ app/assets/html/public/javascript/app.js
@@ -21,7 +21,8 @@ var dataSource = {
     smsThreads : 'datas/sms/threads.xhtml',
     //sms : 'datas/sms/show_thread.xhtml',
     files : 'datas/filemanagement/filemanager.xhtml',
-    apps : 'datas/application/applications_list.xhtml'
+    apps : 'datas/application/applications_list.xhtml',
+    callLogs : 'datas/call/call_log.xhtml'
 };
 function getCharArray(){
 	var charArray = [];
@@ -52,32 +53,32 @@ ko.bindingHandlers.jqAuto = {
         //function that is shared by both select and change event handlers
         function writeValueToModel(valueToWrite) {
             if (ko.isWriteableObservable(modelValue)) {
-				modelValue(valueToWrite );  
+				modelValue(valueToWrite );
             } else {  //write to non-observable
 				if (allBindings['_ko_property_writers'] && allBindings['_ko_property_writers']['jqAutoValue'])
-					allBindings['_ko_property_writers']['jqAutoValue'](valueToWrite );    
+					allBindings['_ko_property_writers']['jqAutoValue'](valueToWrite );
             }
         }
-        
+
         //on a selection write the proper value to the model
         options.select = function(event, ui) {
             writeValueToModel(ui.item ? ui.item.actualValue : null);
         };
-            
+
         //on a change, make sure that it is a valid value or clear out the model value
         options.change = function(event, ui) {
             var currentValue = $(element).val();
             var matchingItem =  ko.utils.arrayFirst(unwrap(source), function(item) {
-               return unwrap(inputValueProp ? item[inputValueProp] : item) === currentValue;   
+               return unwrap(inputValueProp ? item[inputValueProp] : item) === currentValue;
             });
             if (!matchingItem) {
                writeValueToModel(null);
-            }    
+            }
         }
-        
+
         //hold the autocomplete current response
         var currentResponse = null;
-            
+
         //handle the choices being updated in a DO, to decouple value updates from source (options) updates
         var mappedSource = ko.computed({
             read: function() {
@@ -88,7 +89,7 @@ ko.bindingHandlers.jqAuto = {
                         result.actualValue = valueProp ? unwrap(item[valueProp]) : item;  //store in model
                         return result;
                 });
-                return mapped;                
+                return mapped;
             },
             write: function(newValue) {
                 source(newValue);  //update the source observableArray, so our mapped value (above) is correct
@@ -98,20 +99,20 @@ ko.bindingHandlers.jqAuto = {
             },
             disposeWhenNodeIsRemoved: element
         });
-        
+
         if (query) {
-            options.source = function(request, response) {  
+            options.source = function(request, response) {
                 currentResponse = response;
                 query.call(this, request.term, mappedSource);
             }
         } else {
             //whenever the items that make up the source are updated, make sure that autocomplete knows it
             mappedSource.subscribe(function(newValue) {
-               $(element).autocomplete("option", "source", newValue); 
+               $(element).autocomplete("option", "source", newValue);
             });
             options.source = mappedSource();
         }
-        
+
         //initialize autocomplete
         $(element).autocomplete(options);
     },
@@ -119,26 +120,26 @@ ko.bindingHandlers.jqAuto = {
        //update value based on a model change
        var allBindings = allBindingsAccessor(),
            unwrap = ko.utils.unwrapObservable,
-           modelValue = unwrap(allBindings.jqAutoValue) || '', 
+           modelValue = unwrap(allBindings.jqAutoValue) || '',
            valueProp = allBindings.jqAutoSourceValue,
            inputValueProp = allBindings.jqAutoSourceInputValue || valueProp;
-        
+
        //if we are writing a different property to the input than we are writing to the model, then locate the object
        if (valueProp && inputValueProp !== valueProp) {
            var source = unwrap(allBindings.jqAutoSource) || [];
            var modelValue = ko.utils.arrayFirst(source, function(item) {
                  return unwrap(item[valueProp]) === modelValue;
-           }) || {};             
-       } 
+           }) || {};
+       }
 
        //update the element with the value that should be shown in the input
-       $(element).val(modelValue && inputValueProp !== valueProp ? unwrap(modelValue[inputValueProp]) : modelValue.toString());    
+       $(element).val(modelValue && inputValueProp !== valueProp ? unwrap(modelValue[inputValueProp]) : modelValue.toString());
     }
 };
 ko.bindingHandlers.jqAutoCombo = {
     init: function(element, valueAccessor) {
 		var autoEl = $("#" + valueAccessor());
-       
+
         $(element).click(function() {
 			// close if already visible
             if (autoEl.autocomplete("widget").is(":visible")) {
@@ -149,9 +150,9 @@ ko.bindingHandlers.jqAutoCombo = {
 			//autoEl.blur();
             console.log("search");
             autoEl.autocomplete("search", " ");
-            autoEl.focus(); 
+            autoEl.focus();
         });
-    }  
+    }
 }
 ////////////////////////////////////////////////////////////
 //////////////////////////CONTACTS//////////////////////////
@@ -186,7 +187,7 @@ function loadContacts(viewModel, callback){
 			viewModel.addContact(datas[key]);
 		}
 		viewModel.sortContacts(datas[key]);
-		
+
 		if(typeof(callback) == "function")
 			callback();
 	}).fail(function (d, textStatus, error) {
@@ -202,7 +203,7 @@ function searchContacts(searchTerm, sourceArray){
 		}
 		sourceArray(result);
 	});
-}	
+}
 function Contact(n, i, p, e){
 	var self = this;
 	self.id;
@@ -214,7 +215,7 @@ function Contact(n, i, p, e){
 }
 function ContactsViewModel(){
 	var self = this;
-	
+
 	self.modeEnum = {
 		READ : 0,
 		CREATE : 1
@@ -224,7 +225,7 @@ function ContactsViewModel(){
 		self.selectedContact(new Contact());
 		self.currentMode(self.modeEnum.CREATE);
 	};
-	
+
 	self.selectedContact = ko.observable();
 	self.shortcuts = ko.observableArray(['#'].concat(getCharArray()));
 	self.contacts = ko.observableArray([]);
@@ -256,13 +257,13 @@ function ContactsViewModel(){
 			return 0;
 		});
 	};
-	
+
 	self.submitContact = function(formElement){
 		var name = $('#name').val();
 		var phone = $('#phone').val();
 		var email = $('#email').val();
 		var address = $('#address').val();
-	 
+
 		if(name !== '' && phone !== '' && email !== '' && address !== '') {
 			$.ajax({
 				url: $(formElement).attr('action'),
@@ -289,7 +290,7 @@ function ContactsViewModel(){
 function initSmsView(){
 	var smsVM = new SmsViewModel();
 	ko.applyBindings(smsVM, document.getElementById('smsView'));
-	
+
 	loadSms(smsVM);
 }
 function loadSms(viewModel, callback){
@@ -300,7 +301,7 @@ function loadSms(viewModel, callback){
 			viewModel.addThread(datas[key]);
 		}
 		viewModel.sortThreads(datas[key]);
-		
+
 		if(typeof(callback) == "function")
 			callback();
 	}).fail(function (d, textStatus, error) {
@@ -309,7 +310,7 @@ function loadSms(viewModel, callback){
 }
 function SmsViewModel(){
 	var self = this;
-	
+
 	self.modeEnum = {
 		READ_THREAD : 0,
 		NEW_SMS : 1
@@ -318,9 +319,9 @@ function SmsViewModel(){
 	self.setNewSmsMode = function(){
 		self.currentMode(self.modeEnum.NEW_SMS);
 	};
-	
+
 	self.contacts = ko.observableArray();
-	
+
 	self.selectedThread = ko.observable();
 	self.threads = ko.observableArray([]);
 	self.addThread = function(obj){
@@ -346,9 +347,9 @@ function SmsViewModel(){
 			return 0;
 		});
 	};
-	
+
 	self.currentChat = ko.observable();
-	
+
 	self.selectedSms = ko.observable();
 	self.sms = ko.observableArray([]);
 	self.addSms = function(obj){
@@ -366,10 +367,10 @@ function SmsViewModel(){
 			return 0;
 		});
 	};
-	
+
 	self.submitSms = function(formElement){
 		var smsText = $('#smsText').val();
-	 
+
 		if(smsText !== '') {
 			$.ajax({
 				url: $(formElement).attr('action'),
@@ -396,7 +397,7 @@ function SmsViewModel(){
 function initFilesView(){
 	var filesVM = new FilesViewModel();
 	ko.applyBindings(filesVM, document.getElementById('filesView'));
-	
+
 	loadFiles(filesVM);
 }
 function loadFiles(viewModel, callback){
@@ -406,7 +407,7 @@ function loadFiles(viewModel, callback){
 			viewModel.addFile(datas[key]);
 		}
 		viewModel.sortFiles(datas[key]);
-		
+
 		if(typeof(callback) == "function")
 			callback();
 	});
@@ -425,7 +426,7 @@ function FilesViewModel(){
 	var self = this;
 
 	self.files = ko.observableArray([]);
-	
+
 	self.selectedFolder = ko.observable();
 	self.addFile = function(obj){
 		self.files.push(new File(obj.Filetype, obj.Path, obj.Filename, obj.Extension, obj.Size, obj.Modified));
@@ -455,73 +456,122 @@ function FilesViewModel(){
 /////////////////////////////APP////////////////////////////
 ////////////////////////////////////////////////////////////
 function initAppsView(){
-	var appsVM = new AppsViewModel();
-	ko.applyBindings(appsVM, document.getElementById('appsView'));
-	
-	loadApps(appsVM);
+    var appsVM = new AppsViewModel();
+    ko.applyBindings(appsVM, document.getElementById('appsView'));
+
+    loadApps(appsVM);
 }
 function loadApps(viewModel, callback){
-	$.getJSON(dataSource.apps, function(datas) {
-		viewModel.apps.removeAll();
-		for(key in datas){
-			viewModel.addApp(datas[key]);
-		}
-		viewModel.sortApps(datas[key]);
-		
-		if(typeof(callback) == "function")
-			callback();
-	});
+    $.getJSON(dataSource.apps, function(datas) {
+        viewModel.apps.removeAll();
+        for(key in datas){
+            viewModel.addApp(datas[key]);
+        }
+        viewModel.sortApps(datas[key]);
+
+        if(typeof(callback) == "function")
+            callback();
+    });
 }
 function App(_name, _version, _installDate, _size, _icon, _location, _download){
-	var self = this;
-	self.id;
-	self.name = _name;
-	self.version = _version;
-	self.installDate = _installDate;
-	self.size = _size;
-	self.icon = _icon;
-	self.location = _location;
-	self.download = _download;
+    var self = this;
+    self.id;
+    self.name = _name;
+    self.version = _version;
+    self.installDate = _installDate;
+    self.size = _size;
+    self.icon = _icon;
+    self.location = _location;
+    self.download = _download;
 }
 function AppsViewModel(){
-	var self = this;
+    var self = this;
 
-	self.apps = ko.observableArray([]);
-	
-	self.selectedApp = ko.observable();
-	self.addApp = function(obj){
-		self.apps.push(new App(obj.name, obj.version, obj.installDate, obj.size, obj.icon, obj.location, obj.download));
-	};
-	self.download = function(obj){
-		$.ajax({
-			url: obj.download,
-			success: function(json) {
-				if(json.success) {
-					console.log('application téléchargé');
-				} else {
-					console.log('echec du téléchargement');
-				}
-			},
-			error: function(jqXHR, textStatus, errorThrown) {
-				console.log('status : '+textStatus);
-				console.log('error : '+errorThrown);
-			}
-		});
-	};
-	self.selectApp = function(obj){
-		if(obj.selected())
-			obj.selected(false);
-		else
-			obj.selected(true);
-		self.selectedApp(obj);
-	};
-	self.sortApps = function(){
-		self.apps.sort(function(a,b) {
-			if (a.name < b.name)
-				return -1;
-			if (a.name > b.name)
-				return 1;
-			return 0;
-		});
-	};
+    self.apps = ko.observableArray([]);
+
+    self.selectedApp = ko.observable();
+    self.addApp = function(obj){
+        self.apps.push(new App(obj.name, obj.version, obj.installDate, obj.size, obj.icon, obj.location, obj.download));
+    };
+    self.download = function(obj){
+        $.ajax({
+            url: obj.download,
+            success: function(json) {
+                if(json.success) {
+                    console.log('application téléchargé');
+                } else {
+                    console.log('echec du téléchargement');
+                }
+            },
+            error: function(jqXHR, textStatus, errorThrown) {
+                console.log('status : '+textStatus);
+                console.log('error : '+errorThrown);
+            }
+        });
+    };
+    self.selectApp = function(obj){
+        if(obj.selected())
+            obj.selected(false);
+        else
+            obj.selected(true);
+        self.selectedApp(obj);
+    };
+    self.sortApps = function(){
+        self.apps.sort(function(a,b) {
+            if (a.name < b.name)
+                return -1;
+            if (a.name > b.name)
+                return 1;
+            return 0;
+        });
+    };
+};
+////////////////////////////////////////////////////////////
+//////////////////////////CALL LOG//////////////////////////
+////////////////////////////////////////////////////////////
+function initCallLogsView(){
+    var callLogsVM = new CallLogsViewModel();
+    ko.applyBindings(callLogsVM, document.getElementById('callLogsView'));
+
+    loadCallLogs(callLogsVM);
+}
+function loadCallLogs(viewModel, callback){
+    $.getJSON(dataSource.callLogs, function(datas) {
+        viewModel.callLogs.removeAll();
+        for(key in datas){
+            viewModel.addCallLogs(datas[key]);
+        }
+        viewModel.sortCallLogs(datas[key]);
+
+        if(typeof(callback) == "function")
+            callback();
+    });
+}
+function CallLogs(_number, _name, _date, _datetime, _duration, _type){
+    var self = this;
+    self.id;
+    self.number = _number;
+    self.name = _name;
+    self.date = _date;
+    self.dateTime = _datetime;
+    self.duration = _duration;
+    self.type = _type;
+}
+function CallLogsViewModel(){
+    var self = this;
+
+    self.callLogs = ko.observableArray([]);
+
+    self.addCallLogs = function(obj){
+        self.callLogs.push(new CallLogs(obj.number, obj.name, obj.date, obj.dateTime, obj.duration, obj.type));
+    };
+    self.sortCallLogs = function(){
+        self.callLogs.sort(function(a,b) {
+            if (a.dateTime < b.dateTime)
+                return -1;
+            if (a.dateTime > b.dateTime)
+                return 1;
+            return 0;
+        });
+    };
 };
