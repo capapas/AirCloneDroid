03ada744e2101dffb0dd9c42c0e97fce417bc11d
PIERRE Jean-Ernsso <ernsso@hotmail.fr>
2014-07-03 13:51:21 +0200

    delete useless code

--- app/assets/html/public/javascript/app.js
+++ app/assets/html/public/javascript/app.js
@@ -487,43 +487,6 @@ function FilesViewModel(){
 			}
 		});
 	};
-/*
-function explodeTree($array, $delimiter = '_', $baseval = false)
-{
-    if(!is_array($array)) return false;
-    $splitRE   = '/' . preg_quote($delimiter, '/') . '/';
-    $returnArr = array();
-    foreach ($array as $key => $val) {
-        // Get parent parts and the current leaf
-        $parts  = preg_split($splitRE, $key, -1, PREG_SPLIT_NO_EMPTY);
-        $leafPart = array_pop($parts);
-
-        // Build parent structure
-        // Might be slow for really deep and large structures
-        $parentArr = &$returnArr;
-        foreach ($parts as $part) {
-            if (!isset($parentArr[$part])) {
-                $parentArr[$part] = array();
-            } elseif (!is_array($parentArr[$part])) {
-                if ($baseval) {
-                    $parentArr[$part] = array('__base_val' => $parentArr[$part]);
-                } else {
-                    $parentArr[$part] = array();
-                }
-            }
-            $parentArr = &$parentArr[$part];
-        }
-
-        // Add the final part to the structure
-        if (empty($parentArr[$leafPart])) {
-            $parentArr[$leafPart] = $val;
-        } elseif ($baseval && is_array($parentArr[$leafPart])) {
-            $parentArr[$leafPart]['__base_val'] = $val;
-        }
-    }
-    return $returnArr;
-}
-*/
 	self.fileTree = ko.computed(function(){
 		self.sortFiles();
 		var result = {};
